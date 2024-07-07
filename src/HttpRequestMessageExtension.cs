cf610e6388698fcd0de5628a58646f1340683f5c
Jake Soenneker <jake@soenneker.com>
2024-07-07 10:39:22 -0500

    CancellationToken support

--- src/HttpRequestMessageExtension.cs
+++ src/HttpRequestMessageExtension.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Soenneker.Extensions.HttpContent;
 using Soenneker.Extensions.ValueTask;
@@ -10,7 +11,13 @@ namespace Soenneker.Extensions.HttpRequestMessage;
 /// </summary>
 public static class HttpRequestMessageExtension
 {
-    public static async ValueTask<System.Net.Http.HttpRequestMessage> Clone(this System.Net.Http.HttpRequestMessage request)
+    /// <summary>
+    /// Creates a deep clone of the given <see cref="HttpRequestMessage"/> including headers, options, and content.
+    /// </summary>
+    /// <param name="request">The original <see cref="HttpRequestMessage"/> to clone.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A <see cref="ValueTask{TResult}"/> representing the asynchronous operation, with a cloned <see cref="HttpRequestMessage"/> as the result.</returns>
+    public static async ValueTask<System.Net.Http.HttpRequestMessage> Clone(this System.Net.Http.HttpRequestMessage request, CancellationToken cancellationToken = default)
     {
         var clone = new System.Net.Http.HttpRequestMessage(request.Method, request.RequestUri)
         {
@@ -19,7 +26,7 @@ public static class HttpRequestMessageExtension
 
         if (request.Content != null)
         {
-            clone.Content = await request.Content.Clone().NoSync();
+            clone.Content = await request.Content.Clone(cancellationToken: cancellationToken).NoSync();
         }
 
         foreach (KeyValuePair<string, object?> option in request.Options)
