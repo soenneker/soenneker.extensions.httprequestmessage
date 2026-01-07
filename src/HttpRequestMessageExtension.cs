b4dba2ba71fac4d5b79b7f60bd1cd6c977dd4258
Jake Soenneker <jake@soenneker.com>
2026-01-07 14:13:19 -0600

    Perf improvements

--- src/HttpRequestMessageExtension.cs
+++ src/HttpRequestMessageExtension.cs
@@ -1,43 +1,71 @@
+using Soenneker.Extensions.HttpContent;
+using Soenneker.Extensions.ValueTask;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Soenneker.Extensions.HttpContent;
-using Soenneker.Extensions.ValueTask;
 
 namespace Soenneker.Extensions.HttpRequestMessage;
 
-/// <summary>
-/// A collection of helpful HttpRequestMessage extension methods
-/// </summary>
 public static class HttpRequestMessageExtension
 {
     /// <summary>
-    /// Creates a deep clone of the given <see cref="HttpRequestMessage"/> including headers, options, and content.
+    /// Creates a deep copy of the specified <see cref="System.Net.Http.HttpRequestMessage"/> instance, including its
+    /// headers, properties, and content.
     /// </summary>
-    /// <param name="request">The original <see cref="HttpRequestMessage"/> to clone.</param>
-    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
-    /// <returns>A <see cref="ValueTask{TResult}"/> representing the asynchronous operation, with a cloned <see cref="HttpRequestMessage"/> as the result.</returns>
-    public static async ValueTask<System.Net.Http.HttpRequestMessage> Clone(this System.Net.Http.HttpRequestMessage request, CancellationToken cancellationToken = default)
+    /// <remarks>The cloned request will have the same HTTP method, request URI, version, version policy,
+    /// headers, options, and content as the original. If the original request contains content, the content is also
+    /// cloned. The returned request is independent of the original and can be sent or modified separately. Note that
+    /// some properties, such as the request's underlying transport state, are not copied.</remarks>
+    /// <param name="request">The <see cref="System.Net.Http.HttpRequestMessage"/> to clone. Cannot be null.</param>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the asynchronous cloning of the request content. The default
+    /// value is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A <see cref="ValueTask{TResult}"/> that represents the asynchronous operation. The result contains a new <see
+    /// cref="System.Net.Http.HttpRequestMessage"/> instance with the same method, URI, headers, options, and content as
+    /// the original request.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="request"/> is null.</exception>
+    public static ValueTask<System.Net.Http.HttpRequestMessage> Clone(this System.Net.Http.HttpRequestMessage request,
+        CancellationToken cancellationToken = default)
+    {
+        if (request is null)
+            throw new ArgumentNullException(nameof(request));
+
+        // Fast path: no async state machine if there is no content
+        if (request.Content is not null)
+            return CloneWithContent(request, cancellationToken);
+
+        var clone = new System.Net.Http.HttpRequestMessage(request.Method, request.RequestUri)
+        {
+            Version = request.Version,
+            VersionPolicy = request.VersionPolicy
+        };
+
+        foreach (KeyValuePair<string, object?> option in request.Options)
+            clone.Options.TryAdd(option.Key, option.Value);
+
+        foreach (KeyValuePair<string, IEnumerable<string>> header in request.Headers)
+            clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+
+        return new ValueTask<System.Net.Http.HttpRequestMessage>(clone);
+    }
+
+    private static async ValueTask<System.Net.Http.HttpRequestMessage> CloneWithContent(System.Net.Http.HttpRequestMessage request,
+        CancellationToken cancellationToken)
     {
         var clone = new System.Net.Http.HttpRequestMessage(request.Method, request.RequestUri)
         {
-            Version = request.Version
+            Version = request.Version,
+            VersionPolicy = request.VersionPolicy
         };
 
-        if (request.Content != null)
-        {
-            clone.Content = await request.Content.Clone(cancellationToken: cancellationToken).NoSync();
-        }
+        clone.Content = await request.Content!.Clone(cancellationToken: cancellationToken)
+                                     .NoSync();
 
         foreach (KeyValuePair<string, object?> option in request.Options)
-        {
             clone.Options.TryAdd(option.Key, option.Value);
-        }
 
         foreach (KeyValuePair<string, IEnumerable<string>> header in request.Headers)
-        {
             clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
-        }
 
         return clone;
     }
